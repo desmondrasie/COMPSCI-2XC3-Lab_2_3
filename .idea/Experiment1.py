059c1e07f44268b8b3d6bba45b36b9a2f03a5143
desmos <desmond.rasie@live.ca>
2023-01-30 11:07:47 -0500

    Imported code from 'bad sorts' to Experiment 1

---
+++ .idea/Experiment1.py
@@ -0,0 +1,70 @@
+# In the file bad_sorts.py posted alongside this document, you will find implementations of Bubble,
+# Insertion, and Selection sort. Run suitable experiments to compare the runtimes of these three
+# algorithms. In bad_sorts there is a create_random_list function which may be useful. In your report this
+# section should include:
+
+#  An explicit outline of the experiments you ran. That is, list length values, how many “runs”, etc.
+#  A graph of list length vs time displaying the three curves corresponding to the three “bad”
+# sorting algorithms
+#  A brief discussion and conclusion regarding the results. A few sentences are fine here.
+
+
+# Create a random list length "length" containing whole numbers between 0 and max_value inclusive
+def create_random_list(length, max_value):
+    return [random.randint(0, max_value) for _ in range(length)]
+
+
+# Creates a near sorted list by creating a random list, sorting it, then doing a random number of swaps
+def create_near_sorted_list(length, max_value, swaps):
+    L = create_random_list(length, max_value)
+    L.sort()
+    for _ in range(swaps):
+        r1 = random.randint(0, length - 1)
+        r2 = random.randint(0, length - 1)
+        swap(L, r1, r2)
+    return L
+
+# I have created this function to make the sorting algorithm code read easier
+def swap(L, i, j):
+    L[i], L[j] = L[j], L[i]
+
+# **************************************************************************
+# ***** TOSS EVERYTHING ABOVE INTO A MODULE SO ALL EXPERIMENTS CAN USE *****
+# **************************************************************************
+
+# This is the traditional implementation of Insertion Sort.
+def insertion_sort(L):
+    for i in range(1, len(L)):
+        insert(L, i)
+
+
+def insert(L, i):
+    while i > 0:
+        if L[i] < L[i-1]:
+            swap(L, i-1, i)
+            i -= 1
+        else:
+            return
+
+
+# This is the optimization/improvement we saw in lecture
+def insertion_sort2(L):
+    for i in range(1, len(L)):
+        insert2(L, i)
+
+
+def insert2(L, i):
+    value = L[i]
+    while i > 0:
+        if L[i - 1] > value:
+            L[i] = L[i - 1]
+            i -= 1
+        else:
+            L[i] = value
+            return
+    L[0] = value
+
+
+
+
+
